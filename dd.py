534c8500b2c7a8fa67ffe867cb93b6ac8d6d1dac
haeun0210 <102344305+haeun0210@users.noreply.github.com>
2022-05-20 14:18:32 +0900

    Update dd.py

--- dd.py
+++ dd.py
@@ -10,13 +10,13 @@ result = []
 word = ""
 for y in range(len(n)):
     for x in range(len(n[0])):
-        # a 가 있는 위치 찾기
+        # n이  있는 위치 찾기
         if n[y][x] == word[0]:
             # 오른쪽 가로
             check = True
             if x + len(word) - 1 < len(n[0]):
                 for b in range(len(word)):
-                    if a[y][x + b] != word[b]:
+                    if n[y][x + b] != word[b]:
                         check = False
                         break
                 if check:
@@ -34,7 +34,7 @@ for y in range(len(n)):
 
             # 아래로 내려가는 세로
             check = True
-            if y + len(word) - 1 < len(a):
+            if y + len(word) - 1 < len(n):
                 for b in range(len(word)):
                     if n[y + b][x] != word[b]:
                         check = False
@@ -47,6 +47,43 @@ for y in range(len(n)):
             if y - len(word) + 1 >= 0:
                 for b in range(len(word)):
                     if n[y - b][x] != word[b]:
+                        check = False
+                        break
+                if check:
+                    result.append([x, y])
+                    check = True
+            #오른쪽 위 대각선
+            check = True
+            if x + len(word) - 1 < len(n[0]) and  y - len(word) + 1 >= 0:
+                for b in range(len(word)):
+                    if n[y - b][x + b] != word[b]:
+                        check = False
+                        break
+                if check:
+                    result.append([x, y])
+            #왼쪽 위 대각선
+            check = True
+            if x - len(word) + 1 >= 0 and y - len(word) + 1 >= 0:
+                for b in range(len(word)):
+                    if n[y - b][x - b] != word[b]:
+                        check = False
+                        break
+                if check:
+                    result.append([x, y])
+            #오른쪽 아래 대각선
+            check = True
+            if x + len(word) - 1 < len(n[0]) and  y + len(word) - 1 < len(n):
+                for b in range(len(word)):
+                    if n[y + b][x - b] != word[b]:
+                        check = False
+                        break
+                if check:
+                    result.append([x, y])
+            #왼쪽 아래 대각선
+            check = True
+            if x - len(word) + 1 >= 0 and  y + len(word) - 1 < len(n):
+                for b in range(len(word)):
+                    if n[y - b][x - b] != word[b]:
                         check = False
                         break
                 if check:
